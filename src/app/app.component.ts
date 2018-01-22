e36c57ed6a8a0b216d8418fa1feef0f1ae9b4b4a
Adrian - Mihai Petrescu <adpetrescu@deloittece.com>
2018-01-22 17:13:40 +0200

    aws integration and other fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { RESTRICTED_CONFIG, RestrictedConfig } from '../../restricted.config';
+import { AuthService } from '../services/auth.service';
 
 import { ATFHomePage } from '../pages/atf/atfHome/atfHome';
 
@@ -13,7 +14,7 @@ import { ATFHomePage } from '../pages/atf/atfHome/atfHome';
 export class MyApp {
   rootPage:any = ATFHomePage;
   
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, @Inject(RESTRICTED_CONFIG) private restrictedConfig) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, @Inject(RESTRICTED_CONFIG) private restrictedConfig, private authService: AuthService, private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -27,6 +28,16 @@ export class MyApp {
       node.async = true;
       node.charset = 'utf-8';
       document.getElementsByTagName('head')[0].appendChild(node);
+
+      // Auth
+      this.authService.authenticate().catch((error) => {
+        let alert = this.alertCtrl.create({
+          title: 'Authentication failed',
+          subTitle: 'Please close the session and reopen the application.',
+          buttons: ['OK']
+        })
+        alert.present();
+      });  
     });
   }
 }
--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,26 +1,27 @@
 import { Injectable, Inject } from '@angular/core';
-import { HTTP } from '@ionic-native/http';
+import { HTTP, HTTPResponse } from '@ionic-native/http';
 
 import { RESTRICTED_CONFIG, RestrictedConfig } from '../../restricted.config';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class HTTPService {
     
-    constructor(private http: HTTP, @Inject(RESTRICTED_CONFIG) private restrictedConfig) { }
+    constructor(private http: HTTP, @Inject(RESTRICTED_CONFIG) private restrictedConfig, private authService: AuthService) { }
 
-    addTest(body): Promise<void> {
-        var testURL = this.restrictedConfig.apis.postTest.url;
-        var headers = {};
-        this.restrictedConfig.apis.postTest.headers.forEach(header => {
-            headers[header.key] = header.value;
-        });
+    addTest(body): Promise<HTTPResponse> {
+        return new Promise((resolve, reject) => {
+            var testURL = this.restrictedConfig.apis.postTest.url;
+            var headers = {};
+            headers[this.restrictedConfig.auth.apisHeader] = this.authService.token;
 
-        this.http.setDataSerializer("json");
-        
-        return this.http.post(testURL, body, headers).then(response => {
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
+            this.http.setDataSerializer("json");
+            
+            this.http.post(testURL, body, headers).then(response => {
+                resolve(response);
+            }).catch(error => {
+                reject(error);
+            });
         });
     }
 }
