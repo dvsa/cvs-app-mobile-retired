a5ee3b20b02fd5d243f822e9501e622f2d5eb2bf
Tony Cozianu <acozianu@deloittece.com>
2018-12-18 14:52:26 +0200

    CVSB-546 - Fix application.json file path

--- src/pages/testing/test-submitted/test-submitted.ts
+++ src/pages/testing/test-submitted/test-submitted.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { SocialSharing } from '@ionic-native/social-sharing';
-import { TestReportModel } from '../../../models/test-report.model';
 import { TestReportService } from "../../../providers/test-report/test-report.service";
 import { AppConfig } from "../../../../config/app.config";
+import { TestReportModel } from "../../../models/tests/test-report.model";
 
 @IonicPage()
 @Component({
