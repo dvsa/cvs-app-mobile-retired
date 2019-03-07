4f031f3eea81ead47d2b44d525366ac38248e83e
Matt Clarke <mdclarke@deloitte.co.uk>
2019-03-07 08:49:15 +0000

    CVSB-2739

--- src/assets/app-data/eu-vehicle-category/eu-vehicle-category.ts
+++ src/assets/app-data/eu-vehicle-category/eu-vehicle-category.ts
@@ -3,15 +3,15 @@ export class EuVehicleCategoryData {
     return [
       {
         key: "m1",
-        value: "Vehicles designed and constructed for the carriage of passengers and comprising no more than eight seats in addition to the driver's seat."
+        value: "no more than eight seats in addition to the driver's seat"
       },
       {
         key: "m2",
-        value: "Vehicles designed and constructed for the carriage of passengers, comprising more than eight seats in addition to the driver's seat, and having a maximum mass not exceeding 5 tonnes."
+        value: "more than eight seats in addition to the driver's seat, and having a maximum mass not exceeding 5 tonnes"
       },
       {
         key: "m3",
-        value: "Vehicles designed and constructed for the carriage of passengers, comprising more than eight seats in addition to the driver's seat, and having a maximum mass exceeding 5 tonnes."
+        value: "more than eight seats in addition to the driver's seat, and having a maximum mass exceeding 5 tonnes"
       }
     ]
   }
