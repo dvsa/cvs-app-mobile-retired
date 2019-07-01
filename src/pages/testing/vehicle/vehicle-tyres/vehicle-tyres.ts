3864b18257daad7b62a8411ea25a3f5b79e034c0
Alexandru Goncearenco <agoncearenco@deloitte.co.uk>
2019-07-01 23:32:56 +0100

    CVSB-3845

--- src/pages/testing/vehicle/vehicle-tyres/vehicle-tyres.ts
+++ src/pages/testing/vehicle/vehicle-tyres/vehicle-tyres.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { VehicleModel } from '../../../../models/vehicle/vehicle.model';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 import { CommonFunctionsService } from "../../../../providers/utils/common-functions";
-import { APP_STRINGS } from '../../../../app/app.enums';
+import { APP_STRINGS, VEHICLE_TYPE } from '../../../../app/app.enums';
 
 @IonicPage()
 @Component({
@@ -10,6 +10,7 @@ import { APP_STRINGS } from '../../../../app/app.enums';
   templateUrl: 'vehicle-tyres.html',
 })
 export class VehicleTyresPage {
+  VEHICLE_TYPE: typeof VEHICLE_TYPE=VEHICLE_TYPE;
   vehicleData: VehicleModel;
 
   constructor(public navCtrl: NavController,
