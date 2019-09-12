fb27248cbf70b2a302e286da45f3f4dd0322b345
Alexandru Goncearenco <agoncearenco@deloitte.co.uk>
2019-09-12 12:13:16 +0100

    CVSB-3963 Applied PR requested changes

---
+++ src/pages/testing/vehicle/vehicle-brakes/vehicle-brakes.spec.ts
@@ -0,0 +1,62 @@
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {IonicModule, NavController, NavParams, ViewController} from 'ionic-angular';
+import {NavParamsMock} from '../../../../../test-config/ionic-mocks/nav-params.mock';
+import {CommonFunctionsService} from '../../../../providers/utils/common-functions';
+import {ViewControllerMock} from '../../../../../test-config/ionic-mocks/view-controller.mock';
+import {By} from '@angular/platform-browser';
+import {APP_STRINGS, TECH_RECORD_STATUS} from '../../../../app/app.enums';
+import {FormatVrmPipe} from '../../../../pipes/format-vrm/format-vrm.pipe';
+import {VehicleDataMock} from '../../../../assets/data-mocks/vehicle-data.mock';
+import {VehicleBrakesPage} from './vehicle-brakes';
+
+describe('Component: VehicleBrakesPage', () => {
+  let component: VehicleBrakesPage;
+  let componentFixture: ComponentFixture<VehicleBrakesPage>;
+  let navParams: NavParams;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [VehicleBrakesPage, FormatVrmPipe],
+      imports: [IonicModule.forRoot(VehicleBrakesPage)],
+      providers: [
+        NavController,
+        CommonFunctionsService,
+        {provide: NavParams, useClass: NavParamsMock},
+        {provide: ViewController, useClass: ViewControllerMock},
+
+      ]
+    }).compileComponents();
+
+    navParams = TestBed.get(NavParams);
+    spyOn(navParams, 'get').and.returnValue(VehicleDataMock.VehicleData);
+    componentFixture = TestBed.createComponent(VehicleBrakesPage);
+    component = componentFixture.componentInstance;
+  }));
+
+  afterEach(() => {
+    componentFixture.destroy();
+    component = null;
+    navParams = null;
+  });
+
+  it('should not display the provisional label if the techRecord is current', () => {
+    component.vehicleData.techRecord.statusCode = TECH_RECORD_STATUS.CURRENT;
+
+    componentFixture.detectChanges();
+    componentFixture.whenStable().then(() => {
+      let title = componentFixture.debugElement.query(By.css('ion-toolbar ion-title div.toolbar-title'));
+      expect(title).toBeNull();
+    });
+  });
+
+  it('should display the provisional label if the techRecord is provisional', () => {
+    component.vehicleData.techRecord.statusCode = TECH_RECORD_STATUS.PROVISIONAL;
+
+    componentFixture.detectChanges();
+    componentFixture.whenStable().then(() => {
+      let title = componentFixture.debugElement.query(By.css('ion-toolbar ion-title div.toolbar-title'));
+      expect(title.nativeElement.innerText).toBe(APP_STRINGS.PROVISIONAL_LABEL_TEXT);
+    });
+  });
+
+});
