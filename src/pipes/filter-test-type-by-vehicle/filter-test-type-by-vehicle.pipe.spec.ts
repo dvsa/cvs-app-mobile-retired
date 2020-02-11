8565a7ef59098b62a1189991ad13760de950dda7
khodder <khodder@gmail.com>
2020-02-11 22:34:12 +0000

    CVSB-4952 handle cases where values are missing from tech records + vulnerable dependency.

--- src/pipes/filter-test-type-by-vehicle/filter-test-type-by-vehicle.pipe.spec.ts
+++ src/pipes/filter-test-type-by-vehicle/filter-test-type-by-vehicle.pipe.spec.ts
@@ -2,6 +2,7 @@ import { FilterTestTypeByVehiclePipe } from "./filter-test-type-by-vehicle.pipe"
 import { TestTypesReferenceDataModel } from "../../models/reference-data-models/test-types.model";
 import { VehicleDataMock } from "../../assets/data-mocks/vehicle-data.mock";
 import { TestTypesReferenceDataMock } from "../../assets/data-mocks/reference-data-mocks/test-types.mock";
+import {cloneDeep} from "lodash";
 
 describe('FilterTestTypeByVehiclePipe', () => {
   let pipe = new FilterTestTypeByVehiclePipe();
@@ -14,5 +15,29 @@ describe('FilterTestTypeByVehiclePipe', () => {
 
   it('return all items since no vehicle was entered', () => {
     expect((pipe.transform(testTypeData, null)).length).toBe(testTypeData.length);
+  });
+
+  it('handles the situation where String fields are missing without erroring', () => {
+    const modifiedTestData = cloneDeep(testTypeData);
+    modifiedTestData[0].forVehicleConfiguration = ["chicken"];
+    const partialVehicle = cloneDeep(vehicle);
+    delete partialVehicle.techRecord.vehicleConfiguration;
+    expect((pipe.transform(modifiedTestData, partialVehicle)).length).toBe(0);
+  });
+
+  it('handles the situation where Object fields are missing without erroring', () => {
+    const modifiedTestData = cloneDeep(testTypeData);
+    modifiedTestData[0].forVehicleClass = ["chicken"];
+    const partialVehicle = cloneDeep(vehicle);
+    delete partialVehicle.techRecord.vehicleClass;
+    expect((pipe.transform(modifiedTestData, partialVehicle)).length).toBe(1);
+  });
+
+  it('handles the situation where Array fields are missing without erroring', () => {
+    const modifiedTestData = cloneDeep(testTypeData);
+    modifiedTestData[0].forVehicleSubclass = ["chicken"];
+    const partialVehicle = cloneDeep(vehicle);
+    delete partialVehicle.techRecord.vehicleSubclass;
+    expect((pipe.transform(modifiedTestData, partialVehicle)).length).toBe(1);
   });
 });
