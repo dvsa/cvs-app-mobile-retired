d85762c337395e29bdc1a3729084569c5b305ace
Olu Alli <oalli@deloitte.co.uk>
2020-07-27 17:21:18 +0100

    fix(cvsb-9644): prevent the viewing of test history page from crashing when country of registration does not exit

--- src/pages/testing/vehicle/vehicle-history-details/vehicle-history-details.spec.ts
+++ src/pages/testing/vehicle/vehicle-history-details/vehicle-history-details.spec.ts
@@ -1,19 +1,19 @@
-import { VehicleHistoryDetailsPage } from "./vehicle-history-details";
-import { async, ComponentFixture, inject, TestBed } from "@angular/core/testing";
-import { IonicModule, NavController, NavParams, ViewController } from "ionic-angular";
-import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
-import { ViewControllerMock } from "../../../../../test-config/ionic-mocks/view-controller.mock";
-import { NavParamsMock } from "../../../../../test-config/ionic-mocks/nav-params.mock";
-import { CommonFunctionsService } from "../../../../providers/utils/common-functions";
-import { PipesModule } from "../../../../pipes/pipes.module";
-import { TestResultModel } from "../../../../models/tests/test-result.model";
-import { TestResultsHistoryDataMock } from "../../../../assets/data-mocks/test-results-history-data.mock";
-import { FirebaseLogsService } from "../../../../providers/firebase-logs/firebase-logs.service";
-import { FirebaseLogsServiceMock } from "../../../../../test-config/services-mocks/firebaseLogsService.mock";
+import { VehicleHistoryDetailsPage } from './vehicle-history-details';
+import { async, ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { IonicModule, NavController, NavParams, ViewController } from 'ionic-angular';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ViewControllerMock } from '../../../../../test-config/ionic-mocks/view-controller.mock';
+import { NavParamsMock } from '../../../../../test-config/ionic-mocks/nav-params.mock';
+import { CommonFunctionsService } from '../../../../providers/utils/common-functions';
+import { PipesModule } from '../../../../pipes/pipes.module';
+import { TestResultModel } from '../../../../models/tests/test-result.model';
+import { TestResultsHistoryDataMock } from '../../../../assets/data-mocks/test-results-history-data.mock';
+import { FirebaseLogsService } from '../../../../providers/firebase-logs/firebase-logs.service';
+import { FirebaseLogsServiceMock } from '../../../../../test-config/services-mocks/firebaseLogsService.mock';
 import { AppService } from '../../../../providers/global/app.service';
 import { AppServiceMock } from '../../../../../test-config/services-mocks/app-service.mock';
-import { TestTypeService } from "../../../../providers/test-type/test-type.service";
-import { TestTypeServiceMock } from "../../../../../test-config/services-mocks/test-type-service.mock";
+import { TestTypeService } from '../../../../providers/test-type/test-type.service';
+import { TestTypeServiceMock } from '../../../../../test-config/services-mocks/test-type-service.mock';
 
 describe('Component: VehicleHistoryDetailsPage', () => {
   let comp: VehicleHistoryDetailsPage;
@@ -24,21 +24,17 @@ describe('Component: VehicleHistoryDetailsPage', () => {
   let firebaseLogsService: FirebaseLogsService;
 
   beforeEach(async(() => {
-
     TestBed.configureTestingModule({
       declarations: [VehicleHistoryDetailsPage],
-      imports: [
-        IonicModule.forRoot(VehicleHistoryDetailsPage),
-        PipesModule
-      ],
+      imports: [IonicModule.forRoot(VehicleHistoryDetailsPage), PipesModule],
       providers: [
         NavController,
         CommonFunctionsService,
-        {provide: NavParams, useClass: NavParamsMock},
-        {provide: ViewController, useClass: ViewControllerMock},
-        {provide: TestTypeService, useClass: TestTypeServiceMock},
-        {provide: FirebaseLogsService, useClass: FirebaseLogsServiceMock},
-        {provide: AppService, useClass: AppServiceMock}
+        { provide: NavParams, useClass: NavParamsMock },
+        { provide: ViewController, useClass: ViewControllerMock },
+        { provide: TestTypeService, useClass: TestTypeServiceMock },
+        { provide: FirebaseLogsService, useClass: FirebaseLogsServiceMock },
+        { provide: AppService, useClass: AppServiceMock }
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
@@ -90,29 +86,29 @@ describe('Component: VehicleHistoryDetailsPage', () => {
   it('should return true if defects array is not empty', () => {
     defects = [
       {
-        "deficiencyCategory": "major",
-        "deficiencyText": "missing.",
-        "prs": false,
-        "additionalInformation": {
-          "location": {
-            "axleNumber": null,
-            "horizontal": null,
-            "vertical": null,
-            "longitudinal": "front",
-            "rowNumber": null,
-            "lateral": null,
-            "seatNumber": null
+        deficiencyCategory: 'major',
+        deficiencyText: 'missing.',
+        prs: false,
+        additionalInformation: {
+          location: {
+            axleNumber: null,
+            horizontal: null,
+            vertical: null,
+            longitudinal: 'front',
+            rowNumber: null,
+            lateral: null,
+            seatNumber: null
           },
-          "notes": "None"
+          notes: 'None'
         },
-        "itemNumber": 1,
-        "deficiencyRef": "1.1.a",
-        "stdForProhibition": false,
-        "deficiencySubId": null,
-        "imDescription": "Registration Plate",
-        "deficiencyId": "a",
-        "itemDescription": "A registration plate:",
-        "imNumber": 1
+        itemNumber: 1,
+        deficiencyRef: '1.1.a',
+        stdForProhibition: false,
+        deficiencySubId: null,
+        imDescription: 'Registration Plate',
+        deficiencyId: 'a',
+        itemDescription: 'A registration plate:',
+        imNumber: 1
       }
     ];
     expect(comp.checkForDefects(defects)).toBeTruthy;
@@ -123,5 +119,12 @@ describe('Component: VehicleHistoryDetailsPage', () => {
     comp.selectedTestResult.countryOfRegistration = 'a';
     comp.setTestMetadata();
     expect(comp.countryOfRegistration).toEqual('Austria');
+  });
+
+  it('should set "countryOfRegistration on component" to empty or undefined when no matching key', () => {
+    comp.selectedTestResult = {} as TestResultModel;
+    comp.selectedTestResult.countryOfRegistration = 'united';
+    comp.setTestMetadata();
+    expect(comp.countryOfRegistration).toBeUndefined();
   });
 });
