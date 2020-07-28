3f29c52d959558aea3c279144319da812fae458e
Irina Stanciu <istanciu@deloittece.com>
2020-07-28 16:42:32 +0300

    feature/CVSB-16016

--- src/providers/interceptors/auth.interceptor.spec.ts
+++ src/providers/interceptors/auth.interceptor.spec.ts
@@ -1,13 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
-import { AuthInterceptor } from './auth.interceptor';
-import { HTTPService } from '../global/http.service';
-import { Data } from '@angular/router';
-import { AuthService } from '../global/auth.service';
-import { of } from 'rxjs/observable/of';
-import { Store } from '@ngrx/store';
-import { AuthServiceMock } from '../../../test-config/services-mocks/auth-service.mock';
+import { TestBed } from "@angular/core/testing";
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from "@angular/common/http/testing";
+import { HTTP_INTERCEPTORS, HttpClient } from "@angular/common/http";
+import { AuthInterceptor } from "./auth.interceptor";
+import { HTTPService } from "../global/http.service";
+import { Data } from "@angular/router";
+import { AuthService } from "../global/auth.service";
+import { of } from "rxjs/observable/of";
+import { Store } from "@ngrx/store";
+import { AuthServiceMock } from "../../../test-config/services-mocks/auth-service.mock";
 
 export class TestStore {
   dispatch() {}
@@ -18,14 +21,15 @@ describe(`AuthHttpInterceptor`, () => {
   let HttpService: HTTPService;
   let authServiceSpy: any;
   let httpMock: HttpTestingController;
-  const TEST_URL: string = '';
+  const TEST_URL: string = "";
   // dummy hand crafted jwt token for testing purpose only
   const JWT_TOKEN: string =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidXBuIjoidGVzdEBlbWFpbC5jb20ifQ.BlL6ll8xB4iGqDn_KB2mezWRFMHRqbRu-NxDB3443s0';
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvaWQiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwidXBuIjoidGVzdEBlbWFpbC5jb20ifQ.BlL6ll8xB4iGqDn_KB2mezWRFMHRqbRu-NxDB3443s0";
+  let store;
 
   beforeEach(() => {
-    authServiceSpy = jasmine.createSpyObj('AuthService', {
-      getJWTToken: of(JWT_TOKEN)
+    authServiceSpy = jasmine.createSpyObj("AuthService", {
+      getJWTToken: of(JWT_TOKEN),
     });
 
     TestBed.configureTestingModule({
@@ -37,15 +41,16 @@ describe(`AuthHttpInterceptor`, () => {
         {
           provide: HTTP_INTERCEPTORS,
           useClass: AuthInterceptor,
-          multi: true
-        }
-      ]
+          multi: true,
+        },
+      ],
     });
 
     httpClient = TestBed.get(HttpClient);
     HttpService = TestBed.get(HTTPService);
     authServiceSpy = TestBed.get(AuthService);
     httpMock = TestBed.get(HttpTestingController);
+    store = TestBed.get(Store);
   });
 
   afterEach(() => {
@@ -55,12 +60,15 @@ describe(`AuthHttpInterceptor`, () => {
     httpMock = null;
   });
 
-  it('should add an Authentication header', () => {
-    const testData: Data = { name: 'Test Data' };
-    return httpClient.get<Data>(TEST_URL).subscribe((data) => {
+  it("should add an Authentication header", () => {
+    const testData: Data = { name: "Test Data" };
+    spyOn(store, "dispatch");
+    httpClient.get<Data>(TEST_URL).subscribe((data) => {
       expect(data).toEqual(testData);
       const httpRequest = httpMock.expectOne(TEST_URL);
-      expect(httpRequest.request.headers.has('Authorization')).toBeTruthy();
+      expect(httpRequest.request.headers.has("Authorization")).toBeTruthy();
     });
+
+    expect(store.dispatch).toHaveBeenCalled();
   });
 });
