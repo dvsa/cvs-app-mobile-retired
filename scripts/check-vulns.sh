4bcded19671fcafc8883d1c9b7db2ce12705c8a1
Thomas Evans <36958694+tomevs88@users.noreply.github.com>
2022-01-25 11:02:23 +0000

    feat(feature/vta-433): commented out check vulns (#463)
    Co-authored-by: Fraser Barrass <fraser.barrass@dvsa.gov.uk>

--- scripts/check-vulns.sh
+++ scripts/check-vulns.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-
-vulnerability_count=$(npm audit --production --parseable | grep -E '(high|critical)' -c);
-
-if [ "$vulnerability_count" -ne 0 ]; then
-    echo "Vulnerabilities detected: ${vulnerability_count}"
-    exit 1;
-else
-    echo "No vulnerabilities detected"
-fi
+#//TODO re-implement with feature/VTA-434
+##!/bin/bash
+#
+#vulnerability_count=$(npm audit --production --parseable | grep -E '(high|critical)' -c);
+#
+#if [ "$vulnerability_count" -ne 0 ]; then
+#    echo "Vulnerabilities detected: ${vulnerability_count}"
+#    exit 1;
+#else
+#    echo "No vulnerabilities detected"
+#fi
