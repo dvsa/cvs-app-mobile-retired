5c38e6147357dba81113934336eed6c28ed11bec
Fraser Barrass <86603181+FBarrassDVSA@users.noreply.github.com>
2022-02-23 13:56:50 +0000

    feat(VTA-153): added extra context to sentry errors (#464)

--- src/app/sentry-ionic-errorhandler.ts
+++ src/app/sentry-ionic-errorhandler.ts
@@ -3,10 +3,11 @@ import { IonicErrorHandler } from 'ionic-angular';
 import * as Sentry from 'sentry-cordova';
 
 import { VaultService } from '../providers/auth';
+import { VisitService } from '../providers/visit/visit.service';
 
 @Injectable()
 export class SentryIonicErrorHandler extends IonicErrorHandler {
-  constructor(private vaultService: VaultService) {
+  constructor(private vaultService: VaultService, private visitService: VisitService) {
     super();
   }
 
@@ -14,10 +15,20 @@ export class SentryIonicErrorHandler extends IonicErrorHandler {
     super.handleError(error);
 
     const appUser = await this.vaultService.getTesterObsfuscatedId();
+    const testerName = this.visitService.getCurrentTesterName();
+    const atfName = this.visitService.getCurrentATF();
+    const atfPNumber = this.visitService.getCurrentATFPNumber();
+    const vin = this.visitService.getCurrentVIN();
+    const testTypeId = this.visitService.getCurrentTestTypeID();
 
     try {
       Sentry.withScope((scope) => {
         scope.setTag('app-user', appUser);
+        scope.setTag('tester-name', testerName);
+        scope.setTag('test-station', atfName);
+        scope.setTag('aft-p-number', atfPNumber);
+        scope.setTag('vehicle-vin', vin);
+        scope.setTag('test-type-id', testTypeId);
         Sentry.captureException(error.originalError || error);
       });
     } catch (e) {
