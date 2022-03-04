fe25eb4518c6e1b94fee86e8e27c98796756b98f
Tom <thomascrawley99@gmail.com>
2022-03-04 11:26:37 +0000

    feat(vta-461):remove unused import

--- src/providers/global/http.service.ts
+++ src/providers/global/http.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { timeout } from 'rxjs/operators';
 
